f2f0d72c10b5771f11061e046e840456b4b3bc0a
Charlene Campbell-Gray <illest.reps@gmail.com>
2024-12-09 21:20:08 +0000

    added forms.py and logout.html

--- calm_candles/calm_candles/views.py
+++ calm_candles/calm_candles/views.py
@@ -17,3 +17,19 @@ def shipping(request):
 
 def contact_us(request):
     return render(request, 'main/contact_us.html')
+
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
